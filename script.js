34cb9b7e3ae4b1b6f95783eaa3739823e4c2de3d
Vialetta <vialantasv@gmail.com>
2022-03-14 12:08:09 +0300

    Add movies DB

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 'use strict';
 
+const numberOfFilms = prompt('How many movies have you seen?');
+const lastFilm = prompt('What is the last movie you have seen?');
+const lastFilmRating = prompt('How would you rate it?');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+personalMovieDB.movies[lastFilm] = lastFilmRating;
+
+console.log(personalMovieDB);
+
+
